fa6f04acc00b8a27ae0b12430d4b16980f07309b
jihad <jihadmaulana05@gmail.com>
2024-12-13 23:16:18 +0700

    fixing url asettt

--- resources/views/layouts/residence/basetemplate.blade.php
+++ resources/views/layouts/residence/basetemplate.blade.php
@@ -3,14 +3,14 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" type="image/png" sizes="16x16" href="{{ url('assets/plugins/images/pewaca.jpeg') }}">
+  <link rel="icon" type="image/png" sizes="16x16" href="{{ secure_url('assets/plugins/images/pewaca.jpeg') }}">
   <title>Pewaca</title>
   
   <!-- JS -->
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   
   <!-- CSS -->
-  <link href="{{ url('assets/bootstrap/dist/css/bootstrap-5.min.css') }}" rel="stylesheet">
+  <link href="{{ secure_url('assets/bootstrap/dist/css/bootstrap-5.min.css') }}" rel="stylesheet">
   {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> --}}
   <script src="https://cdn.tailwindcss.com"></script>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"/>
@@ -168,7 +168,7 @@
         @include('layouts.residence.bottomnavbar')
     </div>
 
-  <script src="{{ url('assets/bootstrap/dist/js/bootstrap-5.min.js') }}"></script>
+  <script src="{{ secure_url('assets/bootstrap/dist/js/bootstrap-5.min.js') }}"></script>
 
 </body>
 </html>
