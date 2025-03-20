b71b49e1d3e2faa4ebdad86b431e7311ec77bbd9
jihad <jihadmaulana05@gmail.com>
2025-03-20 11:07:35 +0700

    fixing detail warga

--- resources/views/pengurus/warga/detail_warga.blade.php
+++ resources/views/pengurus/warga/detail_warga.blade.php
@@ -82,7 +82,7 @@
                         <img 
                         alt="Belum ada" 
                         class="profile-picture rounded w-32 h-24" 
-                        src="https://storage.googleapis.com/a1aa/image/ZoAiGzvASA4pG9oiGwu50UAjrOG21IrMhFOGfFnKGy1xU85JA.jpg"
+                        src="{{ $warga['marriagePhoto'] }}"
                     /></span>
                 </div>
                 @endif
